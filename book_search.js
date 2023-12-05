8469c641771deaea0f5bcba87d78892828c89ced
Eva Yan <eyan0749@gmail.com>
2023-12-05 02:35:28 +0000

    Add basic functionality and general unit tests

--- book_search.js
+++ book_search.js
@@ -15,23 +15,67 @@
 /**
  * Searches for matches in scanned text.
  * @param {string} searchTerm - The word or term we're searching for.
- * @param {JSON} scannedTextObj - A JSON object representing the scanned text.
+ * @param {JSON} currentBookTextObj - A JSON object representing the scanned text.
  * @returns {JSON} - Search results.
  * */
- function findSearchTermInBooks(searchTerm, scannedTextObj) {
-    /** You will need to implement your search and
-     * return the appropriate object here. */
+ function findSearchTermInBooks(searchTerm, currentBookTextObj) {
+     /** A search term is required to perform a search */
+    if(!searchTerm) {
+        console.log("Please enter a search term.");
+        return {};
+    }
 
     var result = {
-        "SearchTerm": "",
+        "SearchTerm": searchTerm,
         "Results": []
     };
+
+    for (let i = 0; i < currentBookTextObj.length; i++) {
+        const currentBook = currentBookTextObj[i];
+
+        for (let j = 0; j < currentBook.Content.length; j++) {
+            const currentBookContent = currentBook.Content[j];
+            const currentBookText = currentBookContent.Text;
+
+            // Escape special characters in the search string
+            const escapedSearchTerm = searchTerm.replace(/[.*+?^${}()|[\]\\]/g, "\\$&");
+
+            // Create a regular expression with word boundaries and case sensitivity
+            const regex = new RegExp("\\b" + escapedSearchTerm + "\\b");
+
+            // Test if the target string contains the search string
+            if(regex.test(currentBookText)) {
+                result.Results.push({
+                    ISBN: currentBook.ISBN,
+                    Line: currentBookContent.Line,
+                    Page: currentBookContent.Page,
+                });
+                break;
+            }
+        }
+    }
 
     return result;
 }
 
-/** Example input object. */
-const twentyLeaguesIn = [
+/*
+ _   _ _   _ ___ _____   _____ _____ ____ _____ ____
+| | | | \ | |_ _|_   _| |_   _| ____/ ___|_   _/ ___|
+| | | |  \| || |  | |     | | |  _| \___ \ | | \___ \
+| |_| | |\  || |  | |     | | | |___ ___) || |  ___) |
+ \___/|_| \_|___| |_|     |_| |_____|____/ |_| |____/
+
+ */
+
+/* We have provided two unit tests. They're really just `if` statements that
+ * output to the console. We've provided two tests as examples, and
+ * they should pass with a correct implementation of `findSearchTermInBooks`.
+ *
+ * Please add your unit tests below.
+ * */
+
+ /** Test mocks */
+ const twentyLeaguesIn = [
     {
         "Title": "Twenty Thousand Leagues Under the Sea",
         "ISBN": "9780000528531",
@@ -53,49 +97,36 @@ const twentyLeaguesIn = [
             }
         ]
     }
-]
-
-/** Example output object */
-const twentyLeaguesOut = {
-    "SearchTerm": "the",
-    "Results": [
-        {
-            "ISBN": "9780000528531",
-            "Page": 31,
-            "Line": 9
-        }
-    ]
-}
-
-/*
- _   _ _   _ ___ _____   _____ _____ ____ _____ ____
-| | | | \ | |_ _|_   _| |_   _| ____/ ___|_   _/ ___|
-| | | |  \| || |  | |     | | |  _| \___ \ | | \___ \
-| |_| | |\  || |  | |     | | | |___ ___) || |  ___) |
- \___/|_| \_|___| |_|     |_| |_____|____/ |_| |____/
-
- */
-
-/* We have provided two unit tests. They're really just `if` statements that
- * output to the console. We've provided two tests as examples, and
- * they should pass with a correct implementation of `findSearchTermInBooks`.
- *
- * Please add your unit tests below.
- * */
+];
 
 /**
- * Test Helper Function
- * Helps print out unit test results to compare expected versus actual results.
+ * Basic test helper
+ * Print out unit test results to compare expected versus actual results.
  * @param {JSON | number} testResult - JSON object or number representing the result of the test search.
  * @param {JSON | number} expectedResult - JSON object or number representing the correct expected result.
- * @param {String} testIdentifier - String to identify the test; e.g. the name of the test
+ * @param {String} testIdentifier - String to identify the test; e.g. the name of the test.
  * @returns {Void} Prints failure or success message as well as both parameters.
  */
 function printTestResults(testResult, expectedResult, testIdentifier) {
     console.log("Now testing:", testIdentifier);
-    if (JSON.stringify(expectedResult) === JSON.stringify(testResult)) {
+
+    if (typeof value === 'number') {
+        if (JSON.stringify(expectedResult) == JSON.stringify(testResult)) {
+            console.log("PASS");
+        } else {
+            console.log("FAIL");
+            console.log("Expected:", expectedResult);
+            console.log("Received:", testResult);
+        }
+    }
+
+    if (testResult.ISBN == expectedResult.ISBN
+        && testResult.Page == expectedResult.Page
+        && testResult.Line == expectedResult.Page)
+    {
         console.log("PASS");
-    } else {
+    }
+    else {
         console.log("FAIL");
         console.log("Expected:", expectedResult);
         console.log("Received:", testResult);
@@ -105,7 +136,7 @@ function printTestResults(testResult, expectedResult, testIdentifier) {
 
 /** We can check that, given a known input, we get a known output. */
 function testLowerCaseSearchTermReturnsCorrectResults() {
-    const testResult = findSearchTermInBooks("the", twentyLeaguesIn);
+    const testResult = findSearchTermInBooks("he", twentyLeaguesIn);
     const expectedResult =  {
         "SearchTerm": "the",
         "Results": [
@@ -137,7 +168,73 @@ function testLowerCaseSearchTermReturnsCorrectNumberOfResults() {
     printTestResults(testResult.Results.length, expectedResult.Results.length, "testLowerCaseSearchTermReturnsCorrectNumberOfResults");
 }
 
+function testCapitalizedSearchTermReturnsCorrectResults() {
+    const testResult = findSearchTermInBooks("The", twentyLeaguesIn);
+    const expectedResult =  {
+        "SearchTerm": "The",
+        "Results": [
+            {
+                "ISBN": "9780000528531",
+                "Page": 31,
+                "Line": 8
+            }
+        ]
+    };
 
+    printTestResults(testResult, expectedResult, "testCapitalizedSearchTermReturnsCorrectResults");
+}
+
+
+/** We can check that, given a search term, we do not match when it is partially in another word */
+function testSearchTermDoesNotMatchAsSubstring() {
+    const testResult = findSearchTermInBooks("the", twentyLeaguesIn);
+    const expectedResult =  {
+        "SearchTerm": "the",
+        "Results": [
+            {
+                "ISBN": "9780000528531",
+                "Page": 31,
+                "Line": 10
+            }
+        ]
+    };
+
+    printTestResults(testResult, expectedResult, "testSearchTermDoesNotMatchAsSubstring");
+}
+
+/** We can check that given a search term with an apostrophe, it is matched correctly */
+function testSearchTermWithApostropheReturnsCorrectResults() {
+    const testResult = findSearchTermInBooks("Canadian's", twentyLeaguesIn);
+    const expectedResult =  {
+        "SearchTerm": "Canadian's",
+        "Results": [
+            {
+                "ISBN": "9780000528531",
+                "Page": 31,
+                "Line": 9
+            }
+        ]
+    };
+
+    printTestResults(testResult, expectedResult, "testSearchTermWithApostropheReturnsCorrectResults");
+}
+
+/** We can check that a search term matches correctly when located next to punctuation */
+function testSearchTermReturnsCorrectResultsWhenNextToPunctuation() {
+    const testResult = findSearchTermInBooks("profound", twentyLeaguesIn);
+    const expectedResult =  {
+        "SearchTerm": "Canadian's",
+        "Results": [
+            {
+                "ISBN": "9780000528531",
+                "Page": 31,
+                "Line": 9
+            }
+        ]
+    };
+
+    printTestResults(testResult, expectedResult, "testSearchTermReturnsCorrectResultsWhenNextToPunctuation");
+}
 
 
 /** Function to consolidate and run all unit tests in order */
@@ -145,8 +242,12 @@ function runMainTests() {
     // Basic cases
     testLowerCaseSearchTermReturnsCorrectResults();
     testLowerCaseSearchTermReturnsCorrectNumberOfResults();
+    testCapitalizedSearchTermReturnsCorrectResults();
+    testSearchTermDoesNotMatchAsSubstring();
 
-    // Edge cases
+    // Edge cases - Punctuation
+    testSearchTermWithApostropheReturnsCorrectResults();
+    testSearchTermReturnsCorrectResultsWhenNextToPunctuation();
 }
 
 /** Run main test suite */
