de4ed8493f567fef33116b37414a1f6cd9e44ba7
Kim Bergstrom <kimbergstrom78@gmail.com>
2023-12-07 10:19:56 -0700

    Begin pseudo-code

--- book_search.js
+++ book_search.js
@@ -1,33 +1,55 @@
-/** 
+/**
  * RECOMMENDATION
- * 
+ *
  * To test your code, you should open "tester.html" in a web browser.
  * You can then use the "Developer Tools" to see the JavaScript console.
  * There, you will see the results unit test execution. You are welcome
  * to run the code any way you like, but this is similar to how we will
  * run your code submission.
- * 
- * The Developer Tools in Chrome are available under the "..." menu, 
- * futher hidden under the option "More Tools." In Firefox, they are 
- * under the hamburger (three horizontal lines), also hidden under "More Tools." 
+ *
+ * The Developer Tools in Chrome are available under the "..." menu,
+ * futher hidden under the option "More Tools." In Firefox, they are
+ * under the hamburger (three horizontal lines), also hidden under "More Tools."
  */
 
 /**
  * Searches for matches in scanned text.
- * @param {string} searchTerm - The word or term we're searching for. 
+ * @param {string} searchTerm - The word or term we're searching for.
  * @param {JSON} scannedTextObj - A JSON object representing the scanned text.
  * @returns {JSON} - Search results.
- * */ 
- function findSearchTermInBooks(searchTerm, scannedTextObj) {
-    /** You will need to implement your search and 
+ * */
+function findSearchTermInBooks(searchTerm, scannedTextObj) {
+    /** You will need to implement your search and
      * return the appropriate object here. */
+    // Loop through each book in the list
+    // For each book,loop through the content
+    // check contet text for search term
+    // if match, add to result (ISBN, page, line)
+
+    /* Ruby equivalent:
+    def findSearchTermInBooks(search_term, books)
+    result = {"SearchTerm" => search_term, "Results" => []}
+
+    books.each do |book|
+        book["Content"].each do |content|
+        if content["Text"].include?(search_term)
+            result["Results"] << {"ISBN" => book["ISBN"], "Page" => content["Page"], "Line" => content["Line"]}
+        end
+        end
+    end
+
+    return result
+    end
+    */
+
+
 
     var result = {
         "SearchTerm": "",
         "Results": []
     };
-    
-    return result; 
+
+    return result;
 }
 
 /** Example input object. */
@@ -50,11 +72,11 @@ const twentyLeaguesIn = [
                 "Page": 31,
                 "Line": 10,
                 "Text": "eyes were, I asked myself how he had managed to see, and"
-            } 
-        ] 
+            }
+        ]
     }
 ]
-    
+
 /** Example output object */
 const twentyLeaguesOut = {
     "SearchTerm": "the",
@@ -68,18 +90,18 @@ const twentyLeaguesOut = {
 }
 
 /*
- _   _ _   _ ___ _____   _____ _____ ____ _____ ____  
-| | | | \ | |_ _|_   _| |_   _| ____/ ___|_   _/ ___| 
-| | | |  \| || |  | |     | | |  _| \___ \ | | \___ \ 
+ _   _ _   _ ___ _____   _____ _____ ____ _____ ____
+| | | | \ | |_ _|_   _| |_   _| ____/ ___|_   _/ ___|
+| | | |  \| || |  | |     | | |  _| \___ \ | | \___ \
 | |_| | |\  || |  | |     | | | |___ ___) || |  ___) |
- \___/|_| \_|___| |_|     |_| |_____|____/ |_| |____/ 
-                                                      
+ \___/|_| \_|___| |_|     |_| |_____|____/ |_| |____/
+
  */
 
-/* We have provided two unit tests. They're really just `if` statements that 
- * output to the console. We've provided two tests as examples, and 
- * they should pass with a correct implementation of `findSearchTermInBooks`. 
- * 
+/* We have provided two unit tests. They're really just `if` statements that
+ * output to the console. We've provided two tests as examples, and
+ * they should pass with a correct implementation of `findSearchTermInBooks`.
+ *
  * Please add your unit tests below.
  * */
 
@@ -94,7 +116,7 @@ if (JSON.stringify(twentyLeaguesOut) === JSON.stringify(test1result)) {
 }
 
 /** We could choose to check that we get the right number of results. */
-const test2result = findSearchTermInBooks("the", twentyLeaguesIn); 
+const test2result = findSearchTermInBooks("the", twentyLeaguesIn);
 if (test2result.Results.length == 1) {
     console.log("PASS: Test 2");
 } else {
