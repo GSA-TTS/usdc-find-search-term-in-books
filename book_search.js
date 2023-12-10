974f9f59c18acb8705ab3c22e8c1654883677124
Boganabi <logan.ashbaugh0001@gmail.com>
2023-12-09 20:08:56 -0800

    finished assessment: q

--- book_search.js
+++ book_search.js
@@ -22,10 +22,55 @@
     /** You will need to implement your search and 
      * return the appropriate object here. */
 
+    // create the object to return
     var result = {
         "SearchTerm": "",
         "Results": []
     };
+
+    // set the searchTerm parameter (easy enough)
+    result.SearchTerm = searchTerm;
+
+    // iterate through all content objects
+    scannedTextObj.forEach(item => {
+        const content = item.Content;
+        content.forEach((test, i) => {
+            let endWord = ""
+
+            // we can use the search function and not the match function because we only need to return a line once
+            if (test.Text.search(searchTerm) >= 0){
+                result.Results.push({
+                    "ISBN" : item.ISBN,
+                    "Page" : test.Page,
+                    "Line" : test.Line
+                });
+            }
+            else{
+                // check if last element in list has hyphen
+                const endSpace = test.Text.lastIndexOf(" ");
+                if (test.Text.slice(-1) === "-" && endSpace != -1){
+                    endWord = test.Text.slice(endSpace + 1, -1); // store first part of hyphenated word without hyphen
+
+                    // check first part of word in next element
+                    if(content[i + 1]){
+                        const nextLine = content[i + 1].Text;
+                        const spaceIndex = nextLine.indexOf(" ");
+                        if(spaceIndex >= 0){
+                            // get last part of hyphenated word
+                            const startWord = nextLine.slice(0, spaceIndex);
+                            if((endWord + startWord).search(searchTerm) >= 0){
+                                result.Results.push({
+                                    "ISBN" : item.ISBN,
+                                    "Page" : test.Page,
+                                    "Line" : test.Line
+                                });
+                            }
+                        }
+                    }
+                }
+            }
+        });
+    })
     
     return result; 
 }
@@ -67,6 +112,52 @@ const twentyLeaguesOut = {
     ]
 }
 
+/** my own output objects */
+const test1 = {
+    "SearchTerm": "darkness",
+    "Results": [
+        {
+            "ISBN": "9780000528531",
+            "Page": 31,
+            "Line": 8
+        }
+    ]
+}
+
+const test2 = {
+    "SearchTerm" : "asked",
+    "Results": [
+        {
+            "ISBN": "9780000528531",
+            "Page": 31,
+            "Line": 10
+        }
+    ]
+}
+
+const test3 = {
+    "SearchTerm" : "and",
+    "Results": [
+        {
+            "ISBN": "9780000528531",
+            "Page": 31,
+            "Line": 9
+        },
+        {
+            "ISBN": "9780000528531",
+            "Page": 31,
+            "Line": 10
+        }
+    ]
+}
+
+const test4 = {
+    "SearchTerm" : "nonsense",
+    "Results": [
+
+    ]
+}
+
 /*
  _   _ _   _ ___ _____   _____ _____ ____ _____ ____  
 | | | | \ | |_ _|_   _| |_   _| ____/ ___|_   _/ ___| 
@@ -101,4 +192,84 @@ if (test2result.Results.length == 1) {
     console.log("FAIL: Test 2");
     console.log("Expected:", twentyLeaguesOut.Results.length);
     console.log("Received:", test2result.Results.length);
+}
+
+
+/** my own unit tests */
+// test for hyphenated line
+const test3result = findSearchTermInBooks("darkness", twentyLeaguesIn);
+if (JSON.stringify(test1) === JSON.stringify(test3result)) {
+    console.log("PASS: Test 3");
+} else {
+    console.log("FAIL: Test 3");
+    console.log("Expected:", test1);
+    console.log("Received:", test3result);
+}
+
+const test4result = findSearchTermInBooks("darkness", twentyLeaguesIn); 
+if (test4result.Results.length == 1) {
+    console.log("PASS: Test 4");
+} else {
+    console.log("FAIL: Test 4");
+    console.log("Expected:", test1.Results.length);
+    console.log("Received:", test3result.Results.length);
+}
+
+// normal test
+const test5result = findSearchTermInBooks("asked", twentyLeaguesIn);
+if (JSON.stringify(test2) === JSON.stringify(test5result)) {
+    console.log("PASS: Test 5");
+} else {
+    console.log("FAIL: Test 5");
+    console.log("Expected:", test2);
+    console.log("Received:", test5result);
+}
+
+const test6result = findSearchTermInBooks("asked", twentyLeaguesIn); 
+if (test6result.Results.length == 1) {
+    console.log("PASS: Test 6");
+} else {
+    console.log("FAIL: Test 6");
+    console.log("Expected:", test2.Results.length);
+    console.log("Received:", test6result.Results.length);
+}
+
+
+// test for multiple matches
+const test7result = findSearchTermInBooks("and", twentyLeaguesIn);
+if (JSON.stringify(test3) === JSON.stringify(test7result)) {
+    console.log("PASS: Test 7");
+} else {
+    console.log("FAIL: Test 7");
+    console.log("Expected:", test3);
+    console.log("Received:", test7result);
+}
+
+const test8result = findSearchTermInBooks("and", twentyLeaguesIn); 
+if (test8result.Results.length == 2) {
+    console.log("PASS: Test 8");
+} else {
+    console.log("FAIL: Test 8");
+    console.log("Expected:", test3.Results.length);
+    console.log("Received:", test8result.Results.length);
+}
+
+
+// test for no matches
+const test9result = findSearchTermInBooks("nonsense", twentyLeaguesIn);
+if (JSON.stringify(test4) === JSON.stringify(test9result)) {
+    console.log("PASS: Test 9");
+} else {
+    console.log("FAIL: Test 9");
+    console.log("Expected:", test4);
+    console.log("Received:", test9result);
+}
+
+const test10result = findSearchTermInBooks("nonsense", twentyLeaguesIn); 
+if (test10result.Results.length == 0) {
+    console.log("PASS: Test 10");
+} else {
+    console.log("FAIL: Test 10");
+    console.log("Expected:", test4.Results.length);
+    console.log("Received:", test10result.Results.length);
 }
